0f97e9ea5331792bac953506c42bd16f3e9c707f
Dano <danocolombo@gmail.com>
2023-12-29 20:19:30 -0500

    form started

---
+++ src/app/subjects/math.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const MathScreen = () => {
+    return (
+        <View>
+            <Text>Math Screen</Text>
+        </View>
+    );
+};
+
+export default MathScreen;
+
+const styles = StyleSheet.create({});
