9134c3343a171daaee6b037f7c0aa257d5f95d36
Dano <danocolombo@gmail.com>
2024-01-15 11:53:25 -0500

    profile added

--- src/app/VirtualEvent/(tabs)/tabtwo/index.tsx
+++ src/app/VirtualEvent/(tabs)/tabtwo/index.tsx
@@ -5,11 +5,14 @@ import {
     StyleSheet,
     Text,
     View,
+    Image,
 } from 'react-native';
 import React from 'react';
 import { Link, Stack } from 'expo-router';
-import { useAppSelector } from '@/hooks';
+import CustomButton from '@/app/components/CustomButton';
+import users from '@assets/data/users.json';
 
+const user = users[0];
 const TabTwoScreen = () => {
     const APP_NAME = 'VirtualEvent';
     return (
@@ -21,20 +24,22 @@ const TabTwoScreen = () => {
                     headerBackTitle: 'Main',
                 }}
             />
-            <View
-                style={{
-                    flex: 1,
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                }}
-            >
-                <View style={{ paddingHorizontal: 25 }}>
-                    <View style={styles.titleContainer}>
-                        <Text style={styles.title}>Tab Two</Text>
-                    </View>
+            <View style={styles.container}>
+                <Image
+                    source={{ uri: user?.avatarUrl }}
+                    style={styles.avatar}
+                />
+                <Text style={styles.name}>{user?.displayName}</Text>
+                <View style={{ marginTop: 'auto' }}>
+                    <CustomButton
+                        onPress={() => {}}
+                        text='Sign out'
+                        type='TERTIARY'
+                        fgColor='crimson'
+                        bgColor={''}
+                    />
                 </View>
             </View>
-
             <StatusBar style='auto' />
         </SafeAreaView>
     );
@@ -45,7 +50,8 @@ export default TabTwoScreen;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        padding: 10,
+        alignItems: 'center',
     },
     titleContainer: {
         padding: 2,
@@ -68,10 +74,21 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         // width: '70%',
     },
-
     buttonText: {
         textAlign: 'center',
         fontFamily: 'robotoBold',
         fontSize: 14,
+    },
+
+    avatar: {
+        width: 100,
+        aspectRatio: 1,
+        borderRadius: 50,
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 22,
+        marginVertical: 15,
+        color: 'dimgray',
     },
 });
